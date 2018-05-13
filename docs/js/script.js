59feb58360e964b254fa7a1490a70698d6311a43
Jeff Ferrari <jferrari@apple.com>
2018-05-14 02:42:47 +0800

    Big move to new repo: - new post on how to do it - removed tuxillator.github.io - removed cname generator and git deployer

---
+++ docs/js/script.js
@@ -0,0 +1,25 @@
+(function ($) {
+  // Caption
+  $('.entry-content').each(function (i) {
+    $(this).find('img').each(function () {
+      if ($(this).parent().hasClass('fancybox')) return
+
+      var alt = this.alt
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>')
+    })
+
+    $(this).find('.fancybox').each(function () {
+      $(this).attr('rel', 'article' + i)
+    })
+  })
+
+  if ($.fancybox) {
+    $('.fancybox').fancybox()
+  }
+
+  // Mobile nav
+  $('.mobile-nav-panel').click(function () {
+    $('.nav').toggleClass('active')
+  })
+})(jQuery)
